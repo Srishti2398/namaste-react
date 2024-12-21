8ad07750b13855fa3e4093e53a43d3853153d2d8
srishtip <srishti.pandey@aajtak.com>
2024-12-21 17:40:49 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+    "div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"I am h1 tag",),
+    React.createElement("h2",{},"I am h2 tag"),
+    ]),
+    React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"I am h1 tag",),
+    React.createElement("h2",{},"I am h2 tag"),
+    ]),
+    ]);
+
+const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello world from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading); //object
+root.render(parent);
